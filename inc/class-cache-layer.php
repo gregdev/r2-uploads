60d34cb7baeafc019eb26fc17dc5cf1a26d98cb5
Greg Smith <greg@gregdev.com.au>
2024-05-25 15:14:32 +0930

    formatting

--- inc/class-cache-layer.php
+++ inc/class-cache-layer.php
@@ -8,50 +8,58 @@ namespace S3_Uploads;
  * \Aws\CacheIntefrace implementation using WordPress object cache.
  */
 
-class Cache_Layer implements \Aws\CacheInterface, \Countable {
+class Cache_Layer implements \Aws\CacheInterface, \Countable
+{
+    /**
+     * Get object from cache.
+     *
+     * @param string $key Cache key.
+     *
+     * @return mixed|null
+     */
+    public function get($key)
+    {
+        return get_transient($this->get_key($key));
+    }
 
-	/**
-	 * Get object from cache.
-	 *
-	 * @param string $key Cache key.
-	 *
-	 * @return mixed|null
-	 */
-	public function get( $key ) {
-		return get_transient("offload_s3/{$key}");
-	}
+    /**
+     * Set cache value for key.
+     *
+     * @param string $key   Cache key.
+     * @param mixed  $value Cache value.
+     * @param int    $ttl   Cache TTL.
+     *
+     * @return mixed
+     */
+    public function set($key, $value, $ttl = WEEK_IN_SECONDS)
+    {
+        set_transient($this->get_key($key), $value, $ttl);
+    }
 
-	/**
-	 * Set cache value for key.
-	 *
-	 * @param string $key   Cache key.
-	 * @param mixed  $value Cache value.
-	 * @param int    $ttl   Cache TTL.
-	 *
-	 * @return mixed
-	 */
-	public function set( $key, $value, $ttl = WEEK_IN_SECONDS ) {
-		set_transient("offload_s3/{$key}", $value, $ttl);
-	}
+    /**
+     * Remove key from cache.
+     *
+     * @param string $key Cache key.
+     *
+     * @return mixed
+     */
+    public function remove($key)
+    {
+        delete_transient($this->get_key($key));
+    }
 
-	/**
-	 * Remove key from cache.
-	 *
-	 * @param string $key Cache key.
-	 *
-	 * @return mixed
-	 */
-	public function remove( $key ) {
-		delete_transient("offload_s3/{$key}");
-	}
+    /**
+     * Function provided here only for the compatibility with original \Aws\LruArrayCache example.
+     *
+     * @return int
+     */
+    public function count(): int
+    {
+        return 0;
+    }
 
-	/**
-	 * Function provided here only for the compatibility with original \Aws\LruArrayCache example.
-	 *
-	 * @return int
-	 */
-	public function count(): int {
-		return 0;
-	}
-
+    private function get_key(string $key): string
+    {
+        return 'offload_s3/' . sanitize_title($key);
+    }
 }
